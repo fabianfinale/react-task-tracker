63d949f1b67a76d38a91bd28e0876888e39088fc
Fabian Finalé Franqui <fabianfinale@gmail.com>
2021-08-21 17:58:58 -0400

    Add the Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p className='footer__copyright'>
+        Copyright &copy; 2021{' '}
+        <a
+          href='https://github.com/fabianfinale'
+          target='_blank'
+          rel='noreferrer'>
+          Fabian Finalé Franqui
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from './Footer';
 import Header from './Header';
 
 const Layout = () => {
   return (
     <div className='app'>
       <Header />
+      <Footer />
     </div>
   );
 };
